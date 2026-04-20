54f6e843e76de66c18312b1589fbb79161f83d6e
bvasquezkeysije <bvasquezkeysije@gmail.com>
2026-04-19 22:01:35 -0500

    Allow web origin for auth login

--- src/main/java/com/aprendemosya/aprendemosya_api/domain/auth/controller/AuthController.java
+++ src/main/java/com/aprendemosya/aprendemosya_api/domain/auth/controller/AuthController.java
@@ -5,12 +5,19 @@ import com.aprendemosya.aprendemosya_api.domain.auth.dto.LoginRequest;
 import com.aprendemosya.aprendemosya_api.domain.auth.dto.LoginResponse;
 import com.aprendemosya.aprendemosya_api.domain.auth.service.AuthService;
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = {
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://aprendemosya.com",
+        "https://www.aprendemosya.com"
+})
 @RequestMapping("/api/auth")
 public class AuthController {
 
